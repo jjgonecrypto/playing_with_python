175041a0af83312d37f1d4e38d76cf7add680e8b
justin j. moses <justinjmoses@gmail.com>
2012-06-15 15:00:00 -0400

    Adding functionality for spotify play

--- webapp/settings.py
+++ webapp/settings.py
@@ -76,7 +76,7 @@ STATIC_URL = '/static/'
 # Additional locations of static files
 STATICFILES_DIRS = (
     local_path('assets/'),
-    local_path('3rd/')
+    local_path('3rd/'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
