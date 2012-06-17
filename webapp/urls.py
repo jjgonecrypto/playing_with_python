08ef5a864f32a5e51bfc106c31aa02aff4439993
justin j. moses <justinjmoses@gmail.com>
2012-06-17 13:07:54 -0400

    Changing to simpler view implementation

--- webapp/urls.py
+++ webapp/urls.py
@@ -3,20 +3,16 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 admin.autodiscover()
 
+from app.views import IndexView
+
 from app.api import PlaylistResource
 playlist_resource = PlaylistResource()
 
-#import views
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
 urlpatterns = patterns('',
     
-    url(r'^$', 'views.home.homepage', name='home'),
-    
-    (r'^api/', include(playlist_resource.urls)),
+    url(r'^$', IndexView.as_view(), name='index'),
+
+    url(r'^api/', include(playlist_resource.urls)),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
